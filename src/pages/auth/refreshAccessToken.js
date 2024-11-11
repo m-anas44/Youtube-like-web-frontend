fdfa202a7a7ce9ae1ecd1c215e18cf43f45065fa
Muhammad Anas <manasansari1438@gmail.com>
2024-11-11 10:22:21 -0800

    test for prod

--- src/pages/auth/refreshAccessToken.js
+++ src/pages/auth/refreshAccessToken.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL || ""}/api/v1`,
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api/v1",
+  withCredentials: true, // Important for sending cookies
 });
+
 
 axiosInstance.interceptors.response.use(
   (response) => response,
